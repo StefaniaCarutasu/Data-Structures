b572aaed8c523af44eacf29399eded17354c3e35
Stefania Carutasu <62119841+StefaniaCarutasu@users.noreply.github.com>
2020-04-07 02:24:03 +0300

    luat in considerare ca vectorii au aceeasi lungime
    Pe cazul v1.size()<=2 nu luam in considerare faptul ca si v2 are aceeasi dimensiune.

--- Tema_curs/Pb5/Pb5.cpp
+++ Tema_curs/Pb5/Pb5.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-void citire(int &n, vector<int>& v)
+int n;
+void citire(vector<int>& v)
 {
-    cout << "Introduceti lungimea vectorului: ";
-    cin >> n;
     cout << "Introduceti cele " << n << " elemente ";
     for (int i = 0; i < n; i++)
     {
@@ -14,32 +13,18 @@ void citire(int &n, vector<int>& v)
     }
 }
 
-int mediana(vector<int>& v)
+int mediana(vector<int> v)
 {
     if (v.size() % 2 == 0)
         return (v[v.size() / 2] + v[v.size() / 2 - 1]) / 2;
     else return v[v.size() / 2];
 }
 
-int medianaVect(vector<int>& v1, vector<int>& v2)
+int medianaVect(vector<int> v1, vector<int> v2)
 {
     if (v1.size() <= 2)
     {
         vector<int> aux;
-        if (v2.size() % 2 == 0)
-        {
-            aux.push_back(v2[v2.size() / 2]);
-            aux.push_back(v2[v2.size() / 2 - 1]);
-            v2.clear();
-            v2 = aux;
-            aux.clear();
-        }
-        else
-        {
-            aux.push_back(v2[v2.size() / 2]);
-            v2.clear();
-            v2 = aux;
-        }
         unsigned int i = 0, j = 0;
         while (i < v1.size() && j < v2.size())
         {
@@ -64,53 +49,59 @@ int medianaVect(vector<int>& v1, vector<int>& v2)
             aux.push_back(v2[j]);
             j++;
         }
-        if ((aux.size()) % 2 == 0)
-        {
-            return (aux[aux.size() / 2] + aux[aux.size() / 2 - 1]) / 2;
-        }
-        else return aux[(aux.size()) / 2];
+        return (aux[aux.size() / 2] + aux[aux.size() / 2 - 1]) / 2;
     }
     else
     {
         int x = mediana(v1);
         int y = mediana(v2);
-        if (x < y)
+        if (x > y)
         {
             vector<int> aux;
-            for (unsigned int i = 0; i < v1.size()/2; i++)
+            for (unsigned int i = 0; i < v1.size() / 2; i++)
                 aux.push_back(v1[i]);
             v1.clear();
             v1 = aux;
             aux.clear();
-            for (unsigned int i = v2.size()/2+1; i < v2.size(); i++)
+            for (unsigned int i = v2.size() / 2 + 1; i < v2.size(); i++)
                 aux.push_back(v2[i]);
             v2.clear();
             v2 = aux;
         }
         else
         {
-            vector<int> aux;
-            for (unsigned int i = v1.size() / 2 + 1; i < v1.size(); i++)
-                aux.push_back(v1[i]);
-            v1.clear();
-            v1 = aux;
-            aux.clear();
-            for (unsigned int i = 0; i < v2.size()/2; i++)
-                aux.push_back(v2[i]);
-            v2.clear();
-            v2 = aux;
+            if (x < y)
+            {
+                vector<int> aux;
+                for (unsigned i = v1.size() / 2 + 1; i < v1.size(); i++)
+                    aux.push_back(v1[i]);
+                v1.clear();
+                v1 = aux;
+                aux.clear();
+                for (unsigned int i = 0; i < v2.size() / 2; i++)
+                    aux.push_back(v2[i]);
+                v2.clear();
+                v2 = aux;
+            }
+            else return x;
+            
         }
-
+        for (auto& i : v1)
+            cout << i << " ";
+        cout << '\n';
+        for (auto& i : v2)
+            cout << i << " ";
+        cout << '\n';
         return medianaVect(v1, v2);
     }
 }
 
 int main()
 {
-    int m, n;
-    vector<int>v,w;
-    citire(m, v);
-    citire(n, w);
-    cout<<medianaVect(v, w);
+    vector<int>v, w;
+    cout << "Introduceti lungimea vectorului: ";
+    cin >> n;
+    citire(v);
+    citire(w);
+    cout << medianaVect(v, w);
 }
-
