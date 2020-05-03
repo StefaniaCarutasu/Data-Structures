1abe3189530f136e8eaa283adc41d4f73db27a59
Stefania Carutasu <62119841+StefaniaCarutasu@users.noreply.github.com>
2020-05-03 13:26:31 +0300

    reparat 2-cycles

--- Tema4/Graf/Graf.cpp
+++ Tema4/Graf/Graf.cpp
@@ -1,12 +1,29 @@
 #include "directedGraph.h"
+#include<iostream>
+using namespace std;
+
 int main()
 {
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.erase(v.begin());
-	v.push_back(3);
-	for (auto i : v)
-		cout << i <<" ";
+	directedGraph G;
+	int n;
+	cout << "Introduceti numarul de noduri: ";
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+	{
+		G.addNode(i);
+		for (int j = 1; j <= n; j++)
+			if ((j % i == 0 || i % j == 0) && i != j)
+				G.addEdge(i, j);
+	}
+
+	G.initializareVisited();
+	cout << "DFS: ";
+	G.DFS(1);
+	cout << '\n';
+	G.initializareVisited();
+	cout << "BFS: ";
+	G.BFS(1);
+	cout << '\n';
+	cout << "Numarul de two cycles: " << G.twoCycles();
 	return 0;
 }
--- Tema4/Graf/directedGraph.cpp
+++ Tema4/Graf/directedGraph.cpp
@@ -1,19 +1,22 @@
 #include "directedGraph.h"
 
+int directedGraph::noOfNodes = 0;	//initializam numarul de noduri cu 0
+
 void directedGraph::addNode(int a)
 {
-	vector<int> v;
-	listaAdiacenta[a]=v;
+	vector<int> v;	//cream o lista de adiacenta vida initial
+	listaAdiacenta[a]=v;	//adaugam un nou nod in graf
+	noOfNodes++;	//incrementam numarul de noduri ale grafului
 }
 
 void directedGraph::addEdge(int source, int target)
 {
-	listaAdiacenta[source].push_back(target);
+	listaAdiacenta[source].push_back(target);	//adaugam nodul target la lista de adiacenta a nodului source
 }
 
 int directedGraph::hasEdge(int source, int target)
 {
-	for (auto i : listaAdiacenta[source])
+	for (auto i : listaAdiacenta[source])	//parcurgem lista de adiacenta a lui source si vedem daca gasim nodul target
 		if (i == target)
 			return 1;
 	return 0;
@@ -21,11 +24,13 @@ int directedGraph::hasEdge(int source, int target)
 
 void directedGraph::initializareVisited()
 {
-	this->visited[10000] = { false };
+	for (int i = 0; i < 10001; i++)
+		visited[i] = false;
 }
 
 void directedGraph::BFS(int node)
 {
+	initializareVisited();
 	vector<int> queue;
 	visited[node] = true;   //marcam ca vizitat
 	queue.push_back(node);	//adaugam la coada
@@ -42,32 +47,31 @@ void directedGraph::BFS(int node)
 		}
 		queue.erase(queue.begin());		//sterg primul nod din coada si continui algoritmul cu urmatoarul nod
 	}
-	ok = 1;
 }
 
 void directedGraph::DFS(int node)
 {
-	if (ok)
-		initializareVisited();
 	visited[node] = true;
 	cout << node << " ";
 	for (auto i : listaAdiacenta[node])
 		if (!visited[i])
 			DFS(i);
-	ok = 0;
 }
 
 int directedGraph::twoCycles()
 {
-	int contor = 0;
-	for (int i = 0; i < 10000; i++)
-		for (int j = 0; j < 10000; j++)
+	int contor = 0;	//starea initiala: nu avem niciun 2-ciclu
+	vector< vector<int>> verif(noOfNodes+1);
+	for (int i = 0; i <= noOfNodes; i++)
+		for (int j = 0; j <= noOfNodes; j++)
+			verif[i].push_back(0);	//retin daca am verificat daca am muchie intre i si j si intre j si i
+	for (int i = 1; i < noOfNodes + 1; i++)
+		for (int j = 1; j < noOfNodes + 1; j++)
 		{
-			vector<int>::iterator it = find(listaAdiacenta[j].begin(), listaAdiacenta[j].end(), i);
-			vector<int>::iterator itt = find(listaAdiacenta[i].begin(), listaAdiacenta[i].end(), j);
-			if (it != listaAdiacenta[j].end() && itt != listaAdiacenta[i].end())
-				contor++;
+			if(hasEdge(i,j)==1 && hasEdge(j,i)==1 && verif[i][j] == 0) 
+				contor++;	//incrementam numarul de cicluri cu proprietatea din cerinta
+			verif[i][j] = 1; verif[j][i] = 1;	//marcam ca verificat
 		}
 
-	return contor;
+	return contor;	//returnam numarul de 2-cicluri gasite
 }
